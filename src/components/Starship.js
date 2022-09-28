54ce58115cf25cf25c422129de1219fe8322fa09
aprilkrg <aprilkrgonzales@gmail.com>
2022-09-28 08:52:11 -0600

    rendering each starship name

--- src/components/Starship.js
+++ src/components/Starship.js
@@ -2,8 +2,10 @@ import React, { Component } from "react"
 
 export default class Starship extends Component {
     render() {
+        console.log("PROPS:", this.props)
         return(
             <>
+                <h2>{this.props.name}</h2>
             </>
         )
     }

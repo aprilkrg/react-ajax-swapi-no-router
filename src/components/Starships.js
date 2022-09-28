507fbcfb59097f4286a81edfd9d9c66e843d7c66
aprilkrg <aprilkrgonzales@gmail.com>
2022-09-28 08:43:29 -0600

    fetching ships from swapi; set state working

--- src/components/Starships.js
+++ src/components/Starships.js
@@ -1,2 +1,30 @@
 import React, { Component } from "react"
 import axios from "axios"
+
+export default class Starships extends Component {
+    state = {
+        starships: []
+    }
+
+    // componentDidMount = async () => {
+    async componentDidMount() {
+        try {
+            // console.log("THIS:", this)
+            const response = await axios.get('https://swapi.dev/api/starships/')
+			console.log("SHIPS:", response.data.results)
+            this.setState({
+                starships: response.data.results
+            })
+        } catch(err) {
+            console.log(err)
+        }
+    }
+    render() {
+        
+        return(
+            <>
+                <h1>Starships</h1>
+            </>
+        )
+    }
+}
